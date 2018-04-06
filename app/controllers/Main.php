969c78c7a15b38b6f5e40f1b53a3e7f3b9d12da8
jcheron <myaddressmail@gmail.com>
2018-04-06 02:28:26 +0200

    First commit

---
+++ app/controllers/Main.php
@@ -0,0 +1,21 @@
+<?php
+namespace controllers;
+ use Ajax\php\ubiquity\JsUtils;
+
+ /**
+ * Controller Main
+ * @property JsUtils $jquery
+ **/
+class Main extends ControllerBase{
+
+	public function index(){		$semantic=$this->jquery->semantic();
+		$header=$semantic->htmlHeader("header",1);
+		$header->asTitle("Welcome to Ubiquity","Version ".\Ubiquity\core\Framework::version);
+		$bt=$semantic->htmlButton("btTest","Semantic-UI Button");
+		$bt->onClick("$('#test').html('It works with Semantic-UI too !');");
+		\Ubiquity\core\postinstall\Display::semanticMenu("menu",$semantic);
+		$this->jquery->compile($this->view);
+		$this->loadView("index.html");
+	}
+
+}
