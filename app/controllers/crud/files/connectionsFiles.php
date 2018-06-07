810d29dceb36d5b119c0ee4bf10d8a86d9dee6c1
jcheron <myaddressmail@gmail.com>
2018-06-07 02:37:32 +0200

    Update Connections, Add deleteAll action

--- app/controllers/crud/files/connectionsFiles.php
+++ app/controllers/crud/files/connectionsFiles.php
@@ -9,6 +9,11 @@ class ConnectionsFiles extends CRUDFiles{
 	public function getViewIndex(){
 		return "Connections/index.html";
 	}
-
-
+	/**
+	 * {@inheritDoc}
+	 * @see \Ubiquity\controllers\crud\CRUDFiles::getViewBaseTemplate()
+	 */
+	public function getBaseTemplate() {
+		return "base.html";
+	}
 }
